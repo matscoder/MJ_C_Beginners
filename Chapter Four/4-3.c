f34d7919e23d4e2f0b05bebed2e9d04bbf1a52df
matscoder <levin.hsu@outlook.com>
2023-05-17 17:33:11 +0800

    2023/5/17下午新 17:33
    4-3 for  练习4-13写完结束

--- Chapter Four/4-3.c
+++ Chapter Four/4-3.c
@@ -12,7 +12,7 @@
 }
 */
 //代码4-12
-int main()
+/*int main()
 {
 	int a, b;
 	printf("正整数:"), scanf("%d", &a);
@@ -24,14 +24,40 @@ int main()
 		
 
 	return 0;
-}
+}*/
 
 
 //代码4-13
+/*int main()
+{
+	int num, sum=0;  //num输入的整数个数，sum 和  
+	int i ,a;      //a为每次输入的整数
+	printf("输入多少个整数:"), scanf("%d", &num);
+	while (num < 0) {
+		printf("请输入正整数!\n");
+			printf("请重新输入:"), scanf("%d", &num);
+	}
+	for (i = 0; i < num; i++)            //先执行循环体，后执行收尾处理  ，<=一定会多一位，<号时，最后的i=num,<=时,i比num大一位，博客详细写
+	{
+		printf("NO.%d:", i+1 ), scanf("%d", &a);          // i的情况   i=0,1  i=1,2  i=2,3   i=3,4  i=4,5  i=6,  7  
+		sum += a;
+	}
+	printf("合计值:%d\n平均值:%.2f\n", sum, (double)sum / num);
+}*/
+
+
+//练习4-13
 int main()
 {
-	int num, sum, avg;  //num输入的整数个数，sum 和  
-	printf(输入多少个整数:), scanf("%d", &sum);
+	int a, b;
+	int	sum=0;
+	printf("请输入n的值:"), scanf("%d", &a);
 
-
+	for (b = 0; b < a; b++)
+	{
+		printf("NO.%d:%d\t", b + 1,b+1);
+		sum += b + 1;
+	}
+	printf("1到%d的和是%d", a, sum);
+	return 0;
 }
