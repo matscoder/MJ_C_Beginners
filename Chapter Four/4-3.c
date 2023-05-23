15c396eeced7f2a1999167a4f1b90af9c1164138
matscoder <levin.hsu@outlook.com>
2023-05-23 09:11:07 +0800

    2023/5/22
    4-3for语句结束

--- Chapter Four/4-3.c
+++ Chapter Four/4-3.c
@@ -47,31 +47,40 @@
 
 
 //练习4-13
-/*int main()
+int main()
 {
 	int a, b;
 	int	sum=0;
 	printf("请输入n的值:"), scanf("%d", &a);
 
-	for (b = 0; b < a; b++)
+	for (b = 0; b < a; b++)    0,1,1  1,3,2
 	{
 		printf("NO.%d:%d\t", b + 1,b+1);
 		sum += b + 1;
 	}
 	printf("1到%d的和是%d", a, sum);
 	return 0;
-}*/
+}
 
 
 //练习4-14
-/*int main()
+/*int main()    //取余计算
 {
+	int a, b,c;
+	printf("输入1个整数"), scanf("%d", &a);
+	for (b = 1; b <= a; b++)
+	{
+		c = b % 10;
+		printf("%d",c);
+		if (c == 0)
+			printf("\n");
+	}
+	return 0;
+}*/
 
-}
-*/
 
 
-//练习4-15   //先了解清楚计算身高体重的公式
+//练习4-15   //先了解清楚计算身高体重的公式   (i-80  *0.7)
 /*int main()
 {
 	int a, b, c;
@@ -79,8 +88,17 @@
 	printf("结束数值:"), scanf("%d", &  b);
 	printf("间隔数值:"), scanf("%d", & c);
 
+	int g;
+ double t;
+	for (g = a; g <= b; g += c)
+	{
+		t = (g - 80) * 0.7;
+		printf("身高是:%d cm\t 体重是:%.2fkg\n", g,t );
+	}
+	return 0;
 
-}*/
+}
+*/
 
 
 //代码4-14
@@ -103,7 +121,7 @@
 //}
 
 //代码4-15
-int main()
+/*int main()
 {
 	int a, b;
 	printf("请输入1个整数值:"), scanf("%d", &b);
@@ -113,4 +131,69 @@ int main()
 			printf("%d\n\t", a);    
 
 	return 0;
+}*/
+
+//练习4-16
+
+/*int main()
+{
+	int a, b;
+	printf("输入1个整数值:"), scanf("%d", &b);
+	for (a = 1; a < b; a++)
+		//不能用while,否则会无限循环
+		//while (a % 2 == 1)
+			//printf("%d", a);
+		if (a % 2 == 1)
+			printf("%d ", a);
+	return 0;
+}*/
+
+
+//练习4-17
+/*int main()
+{
+	int a, b;
+	printf("输入n的值:"), scanf("%d", &a);
+	for (b = 1; b <= a; b++)
+		printf("%d的二次方是%d \n", b, b * b);
+
+	return 0;
+}*/
+
+//练习4-18
+/*int main()
+{
+	int b,i;
+printf("显示多少个*:"), scanf("%d", &b);
+for (i = 1; i <= b; i++)
+{
+	
+	putchar('*');
+if (i %5== 0)
+		putchar('\n');
 }
+
+return 0;
+}*/
+
+//练习4-19
+/*int main()
+{
+	int a, i;
+	int b = 0;
+	printf("输入整数值:"), scanf("%d", &a);
+
+	for (i = 1; i <= a; i++)
+		/*while (a % i == 0)   //while语句会无限循环第一个整数值的约数1
+		{
+			printf("%d", i);
+			b++;
+		}*/
+
+		/*if (a % i == 0)
+		{
+			printf("%d \n", i);
+			b++;
+		}
+	printf("约数有%d个", b);
+}*/
